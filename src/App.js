fd57f11123beb3ca06408d07d644274210511092
Olive <olivelliott48@gmail.com>
2022-08-22 20:55:02 -0400

    Create the Gallery component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import About from './components/About'
 import Nav from './components/Nav'
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const categories = [
     {
       name: "commercial",
