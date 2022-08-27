4141d6adc36832d15154a1f1a971f2cbc602dfcc
Olive <olivelliott48@gmail.com>
2022-08-26 20:05:02 -0400

    Add conditional rendering to Nav component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import About from './components/About'
 import Nav from './components/Nav'
+import About from './components/About'
 import Gallery from './components/Gallery'
 
 function App() {
+  const [categories] = useState([
+    { name: 'commercial', description: 'photos of grocery stores, food trucks, and other commercial products'},
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
       <main>
         <Gallery></Gallery>
         <About></About>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
-
-function categorySelected(name) {
-    console.log(`${name} clicked`);
-}
-
-function Nav() {
     return (
         <header className='flex-row px-1'>
             <h2>
-                <a data-testid='link' href='/'>
-                    <span role='img' aria-label='camera'>📸</span> Oh Snap!
-                </a>
+            <a data-testid="link" href="/">
+                <span role="img" aria-label="camera"> 📸 </span> Oh Snap!
+            </a>            
             </h2>
             <nav>
                 <ul className='flex-row'>
@@ -38,12 +26,12 @@ function Nav() {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li 
-                        className='mx-1'
-                        key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} 
+                            key={category.name}>
+                            <span onClick={() => {
+                                setCurrentCategory(category)
+                            }} >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
